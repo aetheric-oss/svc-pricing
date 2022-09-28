6a338093d439d49f74caa8e188ac2a133ea535a5
Xipu Li <lixipu98@gmail.com>
2022-09-28 13:27:24 -0700

    Rename client lib mod

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,7 @@
 //! Exposes svc-pricing Client Functions
 
 /// Pricing Client Library: Client Functions, Structs
-pub mod flights {
+pub mod pricing_grpc {
     #![allow(unused_qualifications)]
     include!("grpc.rs");
 }
