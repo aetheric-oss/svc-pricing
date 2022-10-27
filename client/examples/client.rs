161fea3d297e31e161720f668b1cc64932f03e12
Xipu Li <lixipu98@gmail.com>
2022-10-27 16:30:36 -0700

    feat(grpc): add health check and related example

--- client/examples/client.rs
+++ client/examples/client.rs
@@ -3,9 +3,9 @@
 //! This is an example of a gRPC client that connects to Arrow's pricing
 //! server.
 
-use pricing_grpc::pricing_client::PricingClient;
-use pricing_grpc::PricingRequest;
-use svc_pricing_client::pricing_grpc;
+use svc_pricing_client::pricing_grpc::{
+    pricing_client::PricingClient, PricingRequest, ReadyRequest,
+};
 
 /// Example svc-pricing-client.
 ///
@@ -14,7 +14,26 @@ use svc_pricing_client::pricing_grpc;
 /// server
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = PricingClient::connect("http://[::1]:50051").await?;
+    let grpc_port = std::env::var("HOST_PORT_GRPC").unwrap_or_else(|_| "50051".to_string());
+    let mut client = PricingClient::connect(format!("http://[::1]:{grpc_port}")).await?;
+
+    let request = tonic::Request::new(ReadyRequest {});
+    let response = client.is_ready(request).await;
+
+    let mut ok = true;
+    if response.is_err() {
+        ok = false;
+        println!("IsReady: FAIL");
+    } else {
+        println!("IsReady: PASS");
+    }
+
+    if ok {
+        println!("\u{1F9c1} All endpoints responded!");
+    } else {
+        eprintln!("\u{2620} Errors");
+    }
+
     let request = tonic::Request::new(PricingRequest {
         service_type: 0, // 0 for cargo, 1 for rideshare, 2 for charter
         distance: 100.0, // in km
